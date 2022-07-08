ae6f62865059ca257145b66d4862e4d41dc486ac
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-07-08 00:24:40 -0500

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,8 +6,8 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // title
 // Description: - what was your motivation? - why did you build this project? - what problem does it solve? - what did you learn?
 // Table of Contents (optional)- installation, -usage -credits- license
-// Installation - what are the steps required to install your project?
-// USage
+//Badges 
+// How to Contribute
 const questions = [
   {
     type: 'input', 
@@ -15,11 +15,43 @@ const questions = [
     questions: 'What is title of your project?',
     validate: titleInput => {
       if(titleInput){
-        
+        return true;
+        } else {
+            console.log('Please enter a title of your project!');
+            return false;
       }
     }
 
 
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'A short description of the project such as why build the project and the usefulness of this project?',
+    validate: descriptionInput => {
+      if(descriptionInput) {
+        return true;
+      }else{
+        console.log('Please provide a short description of the project!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input', 
+    name: 'installation',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+
+
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use. Please Include screenshots as needed.'
+  }, 
+  {
+   type: 'input',
+   name: 'Credit' 
   }
 
 
